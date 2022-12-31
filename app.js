74c47192a6d9237080e67de48159cd8ae6e1f4ee
tiarnan tuite <tuitetiarnan@gmail.com>
2022-12-31 14:41:51 +0000

    URL for edit and delete pages working

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const port = 3000;
 
-
-//setting up for database connection
+//#region DB Connection
+//setting up database connection
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',      // username root
@@ -29,8 +29,9 @@ function dbConnection(){
         console.log('connected as id ' + connection.threadId);
         });
 }
+//#endregion
 
-//Use body-parser to parse request bodies
+//Use body-parser to get variable url
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
@@ -41,6 +42,7 @@ app.get('/',(req,res)=>{
     res.sendFile(filePath);
 })
 
+//#region employees 
 //employees table
 app.get('/employees', (req, res) => {
 
@@ -84,6 +86,62 @@ app.get('/employees', (req, res) => {
     });
 });
 
+//edit employee
+app.get('/employee/edit/:eid', (req,res)=>{
+    
+    //get parameter from the URL
+    var eid = req.params.eid;
+
+    //select the user with same eid
+    var myQuery = "select * from employee where eid = '" + eid + "'";
+
+    // function updateForm(){
+    //     console.log("hi");
+    // }
+
+    //get employees from database
+    connection.query(myQuery, eid, (error, result) => {
+        if (error) {
+            console.error(error);
+            res.send('Error retrieving employees from database.');
+            return;
+        }else{
+
+            //store user info connected to EID
+            var userInfo = [result[0].ename, result[0].role, result[0].salary];
+
+            //HTML for page
+            let editForm =  "<head>" +
+                            "<title>Edit Employee</title>" +
+                            "<h1>Edit Employee</h1>"+
+                            "</head>";
+
+            //create edit form and store in variable
+            editForm += '<form style="border: 1px solid black; margin-top: 10px; background: linear-gradient(#e66465, #9198e5); width: 30%; border-collapse: separate";>';
+
+            editForm += '<label>EID</label>';
+            editForm += '<input readonly placeholder="'+ eid + '"><br>';
+            editForm += '<label>Name</label>';
+            editForm += '<input type="text" placeholder="'+ userInfo[0] + '"><br>';
+            editForm += '<label>Role</label>';
+            editForm += '<input type="text" placeholder="'+ userInfo[1] + '"><br>';
+            editForm += '<label>Salary</label>';
+            editForm += '<input type="number" placeholder="'+ userInfo[2] + '"><br>';
+
+            //button
+            editForm +='<br><button onClick="updateForm()">Update</button>'
+
+            //link to home page
+            editForm+= '<br><a href="http://localhost:3000/">Home</a>';
+
+            res.send(editForm);
+        }
+    });
+});
+
+//#endregion
+
+//#region departments 
 app.get('/depts', (req, res) => {
 
     var myQuery = "select * from dept";
@@ -114,10 +172,13 @@ app.get('/depts', (req, res) => {
                 table += '<td style="border: 1px solid black;">' + result[i].dname + '</td>';
                 table += '<td style="border: 1px solid black;">' + result[i].budget + '</td>';
                 table += '<td style="border: 1px solid black;">' + result[i].lid + '</td>';
-                table += '<td style="border: 1px solid black;"><a href="http://localhost:3000/departments/delete/' + result[i].did + '">Delete</a></td>';
+                table += '<td style="border: 1px solid black;"><a href="http://localhost:3000/depts/delete/' + result[i].did + '">Delete</a></td>';
                 table += '</tr>';
             }
             table += '</table>';
+
+            //link to home page
+            table+= '<a href="http://localhost:3000/">Home</a>';
             
             //send the table to the page
             res.send(table);
@@ -125,6 +186,40 @@ app.get('/depts', (req, res) => {
         }
     });
 });
+
+//delete department
+app.get('/depts/delete/:did', (req,res)=>{
+    
+    //get parameter from the URL
+    var did = req.params.did;
+
+    //select the user with same eid
+    var myQuery = "select * from dept where did = '" + did + "'";
+
+    //get departments from database
+    connection.query(myQuery, did, (error, result) => {
+        if (error) {
+            console.error(error);
+            res.send('Error retrieving employees from database.');
+            return;
+        }else{
+
+            //store user info connected to EID
+            var deptInfo = [result[0].dname, result[0].lid, result[0].budget];
+
+            let deleteDept =    "<head>" +
+                                "<title>Departments</title>" +
+                                "<h1>Departments</h1>"+
+                                "</head>";
+
+            //link to home page
+            deleteDept += '<br><a href="http://localhost:3000/">Home</a>';
+
+            res.send(deleteDept);
+        }
+    });
+});
+//#endregion
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
